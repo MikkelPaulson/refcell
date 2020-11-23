cf991a84e84d6ea3c4d7f99501c1594eaa3106eb
Mikkel Paulson <git@email.mikkel.ca>
2020-11-23 13:28:33 -0500

    impl deck::Card

--- src/deck.rs
+++ src/deck.rs
@@ -1,3 +1,32 @@
+#[derive(Debug, PartialEq)]
+struct Card(u8, Suit);
+
+impl Card {
+    pub fn new(rank: u8, suit: Suit) -> Result<Card, &'static str> {
+        if rank >= 1 && rank <= 13 {
+            Ok(Card(rank, suit))
+        } else {
+            Err("Invalid rank")
+        }
+    }
+}
+
+#[cfg(test)]
+mod test_card {
+    use super::{Card, Suit};
+
+    #[test]
+    fn new() {
+        assert_eq!(Err("Invalid rank"), Card::new(0, Suit::Hearts));
+        assert_eq!(Ok(Card(1, Suit::Spades)), Card::new(1, Suit::Spades));
+        assert_eq!(Ok(Card(2, Suit::Hearts)), Card::new(2, Suit::Hearts));
+        assert_eq!(Ok(Card(12, Suit::Diamonds)), Card::new(12, Suit::Diamonds));
+        assert_eq!(Ok(Card(13, Suit::Clubs)), Card::new(13, Suit::Clubs));
+        assert_eq!(Err("Invalid rank"), Card::new(14, Suit::Clubs));
+    }
+}
+
+#[derive(Debug, PartialEq)]
 enum Suit {
     Clubs,
     Diamonds,
