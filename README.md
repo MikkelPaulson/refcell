2c71686ef2f06ae7c642b97369b3f8b77bf4902f
Mikkel Paulson <git@email.mikkel.ca>
2020-11-26 18:41:13 -0500

    add CLI
    Add a command-line version of the game. At least now there's something
    playable.

--- README.md
+++ README.md
@@ -1,2 +1,31 @@
 # RefCell
 A Freecell implementation in Rust.
+
+## How to play
+
+There are two binaries, one with a GUI created using
+[Druid](https://github.com/linebender/druid/), the other a command-line
+application. They both sit on top of game logic implemented at the data layer.
+
+Running both of them requires Rust and Cargo to be installed on your local
+environment.
+
+### GUI
+
+The GUI is incomplete and very, very broken.
+
+    cargo run --bin gui
+
+### CLI
+
+The CLI works, although it doesn't provide convenience features like
+automatically moving multiple cards at once. You will need a terminal capable of
+displaying Unicode characters.
+
+    cargo run --bin cli
+
+Type the character for the source position followed by the destination position.
+For instance, to move from the third column to the first free cell, type "3a".
+
+The game will automatically end when there are no cards of higher rank on top
+of cards of lower rank. You can end it prematurely by pressing ^C.
---
+++ src/bin/cli.rs
@@ -0,0 +1,177 @@
+use refcell::data::*;
+
+use std::io;
+use std::str;
+
+fn main() {
+    let mut tableau = Tableau::deal(Deck::shuffled());
+
+    println!("{}", tableau);
+    println!("Type the character for the source position followed by the destination position.\nFor instance, to move from the third column to the first free cell, type \"3a\".");
+
+    while !tableau.is_won() {
+        let mut buffer = String::new();
+        io::stdin().read_line(&mut buffer).expect("I/O error");
+
+        match buffer.parse::<Action>() {
+            Ok(action) => {
+                if let Some(card) = match action.from {
+                    Coordinate::Cascade(n) => tableau.cascades[n as usize].pop(),
+                    Coordinate::Cell(n) => tableau.cells[n as usize].take(),
+                    Coordinate::Foundation(_) => unreachable!(),
+                } {
+                    if let Err((card, message)) = match action.to {
+                        Coordinate::Cascade(n) => tableau.cascades[n as usize].push(card),
+                        Coordinate::Cell(n) => tableau.cells[n as usize].push(card),
+                        Coordinate::Foundation(n) => tableau.foundations[n as usize].push(card),
+                    } {
+                        match action.from {
+                            Coordinate::Cascade(n) => {
+                                tableau.cascades[n as usize].push_unchecked(card)
+                            }
+                            Coordinate::Cell(n) => tableau.cells[n as usize].push(card).unwrap(),
+                            Coordinate::Foundation(_) => unreachable!(),
+                        }
+
+                        println!("{}", message);
+                    }
+                } else {
+                    println!("That space is empty.");
+                }
+            }
+            Err(message) => println!("{}", message),
+        }
+        println!("{}", tableau);
+    }
+
+    println!("You win!");
+}
+
+#[derive(Debug, PartialEq)]
+struct Action {
+    from: Coordinate,
+    to: Coordinate,
+}
+
+impl str::FromStr for Action {
+    type Err = &'static str;
+
+    fn from_str(s: &str) -> Result<Self, Self::Err> {
+        let s = s.trim();
+        if s.len() == 2 {
+            let from = s[0..1].parse()?;
+            let to = s[1..2].parse()?;
+
+            if from == to {
+                Err("The source and destination are the same.")
+            } else if let Coordinate::Foundation(_) = from {
+                Err("You cannot take a card from a foundation.")
+            } else {
+                Ok(Action { from, to })
+            }
+        } else {
+            Err("Invalid input.")
+        }
+    }
+}
+
+#[cfg(test)]
+mod test_action {
+    use super::{Action, Coordinate};
+
+    #[test]
+    fn valid() {
+        assert_eq!(
+            Ok(Action {
+                from: Coordinate::Cascade(0),
+                to: Coordinate::Cell(0)
+            }),
+            "1a\n".parse::<Action>(),
+        );
+    }
+
+    #[test]
+    fn invalid() {
+        assert_eq!(Err("Invalid input."), "0a\n".parse::<Action>());
+        assert_eq!(Err("Invalid input."), "aa\n".parse::<Action>());
+        assert_eq!(Err("Invalid input."), "\n".parse::<Action>());
+        assert_eq!(Err("Invalid input."), "1a1\n".parse::<Action>());
+        assert_eq!(Err("Invalid input."), "".parse::<Action>());
+    }
+}
+
+#[derive(Debug, PartialEq)]
+enum Coordinate {
+    Cascade(u8),
+    Cell(u8),
+    Foundation(u8),
+}
+
+impl str::FromStr for Coordinate {
+    type Err = &'static str;
+
+    fn from_str(s: &str) -> Result<Self, Self::Err> {
+        if let Some(c) = s.chars().nth(0) {
+            match c {
+                '1'..='8' => Ok(Coordinate::Cascade(c.to_digit(10).unwrap() as u8 - 1)),
+                'a'..='d' | 'A'..='D' => Ok(Coordinate::Cell(c.to_digit(36).unwrap() as u8 - 10)),
+                'w'..='z' | 'W'..='Z' => {
+                    Ok(Coordinate::Foundation(c.to_digit(36).unwrap() as u8 - 32))
+                }
+                _ => Err("Invalid input."),
+            }
+        } else {
+            Err("Invalid input.")
+        }
+    }
+}
+
+#[cfg(test)]
+mod test_coordinate {
+    use super::Coordinate;
+
+    #[test]
+    fn cascade() {
+        assert_eq!(Ok(Coordinate::Cascade(0)), "1".parse::<Coordinate>());
+        assert_eq!(Ok(Coordinate::Cascade(1)), "2".parse::<Coordinate>());
+        assert_eq!(Ok(Coordinate::Cascade(2)), "3".parse::<Coordinate>());
+        assert_eq!(Ok(Coordinate::Cascade(3)), "4".parse::<Coordinate>());
+        assert_eq!(Ok(Coordinate::Cascade(4)), "5".parse::<Coordinate>());
+        assert_eq!(Ok(Coordinate::Cascade(5)), "6".parse::<Coordinate>());
+        assert_eq!(Ok(Coordinate::Cascade(6)), "7".parse::<Coordinate>());
+        assert_eq!(Ok(Coordinate::Cascade(7)), "8".parse::<Coordinate>());
+    }
+
+    #[test]
+    fn cell() {
+        assert_eq!(Ok(Coordinate::Cell(0)), "A".parse::<Coordinate>());
+        assert_eq!(Ok(Coordinate::Cell(0)), "a".parse::<Coordinate>());
+        assert_eq!(Ok(Coordinate::Cell(1)), "B".parse::<Coordinate>());
+        assert_eq!(Ok(Coordinate::Cell(1)), "b".parse::<Coordinate>());
+        assert_eq!(Ok(Coordinate::Cell(2)), "C".parse::<Coordinate>());
+        assert_eq!(Ok(Coordinate::Cell(2)), "c".parse::<Coordinate>());
+        assert_eq!(Ok(Coordinate::Cell(3)), "D".parse::<Coordinate>());
+        assert_eq!(Ok(Coordinate::Cell(3)), "d".parse::<Coordinate>());
+    }
+
+    #[test]
+    fn foundation() {
+        assert_eq!(Ok(Coordinate::Foundation(0)), "W".parse::<Coordinate>());
+        assert_eq!(Ok(Coordinate::Foundation(0)), "w".parse::<Coordinate>());
+        assert_eq!(Ok(Coordinate::Foundation(1)), "X".parse::<Coordinate>());
+        assert_eq!(Ok(Coordinate::Foundation(1)), "x".parse::<Coordinate>());
+        assert_eq!(Ok(Coordinate::Foundation(2)), "Y".parse::<Coordinate>());
+        assert_eq!(Ok(Coordinate::Foundation(2)), "y".parse::<Coordinate>());
+        assert_eq!(Ok(Coordinate::Foundation(3)), "Z".parse::<Coordinate>());
+        assert_eq!(Ok(Coordinate::Foundation(3)), "z".parse::<Coordinate>());
+    }
+
+    #[test]
+    fn invalid() {
+        assert_eq!(Err("Invalid input."), "9".parse::<Coordinate>());
+        assert_eq!(Err("Invalid input."), "0".parse::<Coordinate>());
+        assert_eq!(Err("Invalid input."), "E".parse::<Coordinate>());
+        assert_eq!(Err("Invalid input."), "!".parse::<Coordinate>());
+        assert_eq!(Err("Invalid input."), "".parse::<Coordinate>());
+    }
+}
